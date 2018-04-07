dbc8ce2edb35c84e297460b68e1d44f8f1115e44
Moulton <moulton0@gmail.com>
2018-04-07 10:32:03 -0700

    Swapped to the starting guide used in the course

--- apm/e2e/app.e2e-spec.ts
+++ apm/e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { AppPage } from './app.po';
+import { APMPage } from './app.po';
 
 describe('apm App', () => {
-  let page: AppPage;
+  let page: APMPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    page = new APMPage();
   });
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to Angular: Getting Started!!');
   });
 });
