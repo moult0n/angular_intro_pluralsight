e798a48d1f68b245ec103a25c150c6245f084189
Moulton <moulton0@gmail.com>
2018-04-08 10:42:23 -0700

    finished up addition routing techniques

--- apm-app/src/app/app.module.ts
+++ apm-app/src/app/app.module.ts
@@ -1,24 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
-
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-
 import { StarComponent } from './shared/star.component';
-
 import { ProductService } from './products/product.service';
-
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDetailComponent } from './products/product-detail.component';
-
 import { WelcomeComponent } from './home/welcome.component';
-
 import { RouterModule } from '@angular/router';
+import { ProductGuardService } from './products/product-guard.service';
 
 @NgModule({
   declarations: [
@@ -35,13 +27,17 @@ import { RouterModule } from '@angular/router';
     HttpClientModule,
     RouterModule.forRoot([
       {path: 'products', component: ProductListComponent},
-      {path: 'product/:id', component: ProductDetailComponent},
+      {path: 'products/:id', 
+              canActivate: [ProductGuardService],
+              component: ProductDetailComponent},
       {path: 'welcome', component: WelcomeComponent},
       {path: '', redirectTo: 'welcome', pathMatch: 'full'}, 
       {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
     ])
   ],
-  providers: [ProductService],
+  providers: [ProductService,
+    ProductGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
